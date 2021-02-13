4ad623d7424cb8c2b205c54949ae953d5fe2b0e9
Aarush-Goyal <goyalaarush92@gmail.com>
2021-02-13 17:39:26 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import AppBar from "./components/AppBar";
-import FAB from "./components/FAB";
-import BlogGrid from "./components/BlogGrid";
+import { Navbar } from "./components/Drawer";
 function App() {
   return (
-    <main>
-      <AppBar />
-      <div className="edge-spacing">
-        <BlogGrid />
-      </div>
-      <FAB />
-    </main>
+    <>
+      <Navbar />
+    </>
   );
 }
 
--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,67 +1,74 @@
 import React from "react";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Avatar from "@material-ui/core/Avatar";
-import Divider from "@material-ui/core/Divider";
-import Typography from "@material-ui/core/Typography";
-import SettingsIcon from "@material-ui/icons/Settings";
-import AccountBoxIcon from "@material-ui/icons/AccountBox";
-import AssessmentIcon from "@material-ui/icons/Assessment";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import Badge from "@material-ui/core/Badge";
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-export default function SideDrawer(props) {
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  Input,
+  useDisclosure,
+  Flex,
+  Text,
+  Spacer,
+} from "@chakra-ui/react";
+
+import {} from "@chakra-ui/react";
+import { FaBars } from "react-icons/fa";
+
+export const DrawerButton = ({ btnRef, onOpen }) => {
+  return (
+    <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+      <FaBars />
+    </Button>
+  );
+};
+
+export function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
     <>
-      <Drawer anchor="left" open={props.open} onClose={props.onClose}>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar variant="rounded">
-                <Typography variant="h5">L</Typography>
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText>
-              <Typography variant="h5">Lorem Ipsum</Typography>
-            </ListItemText>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemIcon>
-              <AccountBoxIcon />
-            </ListItemIcon>
-            <ListItemText>Manage your account</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <AssessmentIcon />
-            </ListItemIcon>
-            <ListItemText>View your posts</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <Badge badgeContent={10} color="primary">
-                <NotificationsIcon />
-              </Badge>
-            </ListItemIcon>
-            <ListItemText>Notifications</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText>Settings</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <ExitToAppIcon />
-            </ListItemIcon>
-            <ListItemText>Sign out</ListItemText>
-          </ListItem>
-        </List>
+      <Flex
+        w="100%"
+        h="10vh"
+        // bg="blue.300"
+        justify="space-evenly"
+        align="center"
+        shadow="base"
+      >
+        <Flex w="30%" justify="space-evenly" align="center">
+          <DrawerButton btnRef={btnRef} onOpen={onOpen} />
+          <Text fontSize="2xl">Your Name</Text>
+        </Flex>
+        <Spacer />
+      </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Create your account</DrawerHeader>
+
+            <DrawerBody>
+              <Input placeholder="Type here..." />
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button color="blue">Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
       </Drawer>
     </>
   );
